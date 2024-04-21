946bc72f680adcb799fd8072fc9f9085f2e4600f
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2024-04-21 11:06:52 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,17 +16,25 @@ then
     echo "pip3 could not be found. Attempting to install..."
     sudo apt update
     sudo apt install -y python3-pip
-
-    # Install Python packages
-    echo "Installing Python packages..."
-
-    pip3 install yfinance alpaca-trade-api sqlalchemy pytz ta-lib schedule
-
 fi
 
-# Install TA-Lib dependencies
-echo "Installing TA-Lib dependencies ..."
+# Create and activate virtual environment in the user's home directory
+HOME_VENV_PATH="$HOME/My-Python-Virtual-Environment-Packages"
+VENV_PATH="$HOME_VENV_PATH/venv"
 
+# Check if the virtual environment directory exists, if not, create it
+if [ ! -d "$VENV_PATH" ]; then
+    mkdir -p "$VENV_PATH"
+fi
+
+# Activate the virtual environment
+source "$VENV_PATH/bin/activate"
+
+# Install Python packages within the virtual environment
+pip3 install yfinance alpaca-trade-api sqlalchemy pytz ta-lib schedule
+
+# Install TA-Lib dependencies  
+echo "Installing TA-Lib dependencies ..."
 sudo apt-get install libatlas-base-dev gfortran -y
 
 # Download and install TA-Lib
@@ -44,9 +52,15 @@ cd ..
 rm -r ta-lib
 rm ta-lib-0.4.0-src.tar.gz
 
+# Deactivate the virtual environment
+deactivate
+
 echo "All done! You can now run your Python script."
 
 # Inform the user about the virtual environment
 echo "Your Python commands in the directory named My-Python-Virtual-Environment-Packages will be the Python commands that run this installed virtual environment's Python programs."
 
 echo "just open the folder named My-Python-Virtual-Environment-Packages/venv/bin and run pip3 to install anything else with the command ./pip3 and then run python3 with the command ./python3  "
+
+# Inform the user about the virtual environment
+echo "Your Python commands in the directory named My-Python-Virtual-Environment-Packages will be the Python commands that run this installed virtual environment's Python programs."
